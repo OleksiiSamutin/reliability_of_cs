32ef95d9213956f6772983c2243a2817105f5cb0
Oleksii Samutin <45272001+OleksiiSamutin@users.noreply.github.com>
2022-04-11 15:11:24 +0300

    Made second lab

---
+++ lab2/main.js
@@ -0,0 +1,73 @@
+const INIT_P = [0.69, 0.62, 0.53, 0.32, 0.33, 0.28];
+const ADJANCENCY_MATRIX = [
+    [0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+];
+
+let resP = 0;
+
+const generateTruthTable = (INIT_P) => {
+    const res = [];
+    for (let i = (Math.pow(2, INIT_P.length) - 1); i >= 0; i--) {
+        const row = [];
+        for (let j = (INIT_P.length - 1); j >= 0; j--) {
+            row[j] = (i & Math.pow(2, j)) ? 1 : 0
+        }
+        res.push(row)
+    }
+    return res;
+}
+const truthTable = generateTruthTable(INIT_P);
+
+
+const deepSearch = (m, row = 0, res =
+    new Array(m.length).fill(1, 0, 1).fill(0, 1, m.length), cache = []
+) => {
+    if (m[row].filter(el => el !== 0).length === 0) {
+        cache.push(res)
+    }
+    m[row].forEach((el, i, array) => {
+        if (el === 1) {
+            let result = [...res];
+            result[i] = 1;
+            deepSearch(m, i, result, cache);
+        }
+    })
+    return cache
+}
+
+const critPaths = deepSearch(ADJANCENCY_MATRIX);
+const resOfTruthElem = new Set();
+truthTable.forEach(truthEl => {
+    critPaths.forEach(critPath => {
+        const numberOfOnes = critPath.filter(el => el === 1).length;
+        let counter = 0;
+        critPath.forEach((el, i, array) => {
+            if (el === 1 && truthEl[i] === 1) {
+                counter++
+            }
+        })
+        if (counter === numberOfOnes) {
+            resOfTruthElem.add(truthEl);
+        }
+    })
+})
+
+resOfTruthElem.forEach(path => {
+    let multiplyQ;
+    path.forEach((el, i) => {
+        if (el === 1) {
+            if (!multiplyQ) {
+                multiplyQ = INIT_P[i]
+            } else {
+                multiplyQ *= INIT_P[i]
+            }
+        }
+    })
+    resP += multiplyQ
+})
+console.log(resP)
